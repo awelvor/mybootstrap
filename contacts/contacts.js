43d01f602a032b0b69808f6105a441f9e0482eb1
awelvor <philippe.regent@gmail.com>
2024-06-21 14:28:17 +0200

    Update contacts.js

--- contacts/contacts.js
+++ contacts/contacts.js
@@ -1,10 +1,11 @@
 /* contacts.js */
 
 tab=[
-    {prenom:"Philippe", nom:"REGENT", dat:"2024-06-21"}, 
-    {prenom:"Nicole", nom:"REGENT", dat:"2024-06-20"} , 
-    {prenom:"Pascal", nom:"REGENT", dat:"2024-06-21"}, 
-    {prenom:"Guillaume", nom:"REGENT", dat:"2024-06-20"} , 
+    {prenom:"Jacques", nom:"BIGOU", dn:"23-05-1942",dat:"2024-06-21"}, 
+    {prenom:"Philippe", nom:"REGENT", dn:"07-09-1947", dat:"2024-06-21"}, 
+    {prenom:"Nicole", nom:"REGENT", dn:"14-11-1947", dat:"2024-06-20"} , 
+    {prenom:"Pascal", nom:"REGENT",dn:"13-02-1976", dat:"2024-06-21"}, 
+    {prenom:"Guillaume", nom:"REGENT", dn:"03-10-1977", dat:"2024-06-20"} , 
     ];
     
     /* end of file */
