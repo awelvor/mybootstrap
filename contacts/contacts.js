5a267c31e1d4188df3e040b8d572e672463b6525
awelvor <philippe.regent@gmail.com>
2024-06-22 20:49:54 +0200

    Update contacts.js

--- contacts/contacts.js
+++ contacts/contacts.js
@@ -6,6 +6,40 @@ tab=[
     {prenom:"Nicole", nom:"REGENT", dn:"14-11-1947", dat:"2024-06-20"} , 
     {prenom:"Pascal", nom:"REGENT",dn:"13-02-1976", dat:"2024-06-21"}, 
     {prenom:"Guillaume", nom:"REGENT", dn:"03-10-1977", dat:"2024-06-20"} , 
+    {prenom:"Marie-Cécile", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Camille", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Léa", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Juliette", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Soline", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Maxence", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Côme", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Antonin", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Faustine", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Marie-Gabrielle", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Etienne", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Baudouin", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Louis-Marie", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"François-Guillaume", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Xavier", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Dominique", nom:"REGENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"", nom:"THIEFFRY", dn:"", dat:"2024-06-22"} , 
+    {prenom:"", nom:"THIEFFRY", dn:"", dat:"2024-06-22"} , 
+    {prenom:"", nom:"TIBERGHIEN", dn:"", dat:"2024-06-22"} , 
+    {prenom:"", nom:"TIBERGHIEN", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Jacques-Yves", nom:"VINCENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Jalila", nom:"VINCENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Patrick", nom:"VINCENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Marielle", nom:"VINCENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Pascal", nom:"VINCENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Myriam", nom:"VINCENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Pierre", nom:"VINCENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Marie-Hélène", nom:"VINCENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Aude", nom:"VINCENT", dn:"", dat:"2024-06-22"} , 
+    {prenom:"Benoit", nom:"VINCENT", dn:"", dat:"2024-06-22"} , 
+    
+    
+
+    
     ];
     
     /* end of file */
