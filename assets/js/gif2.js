c2028ce50006dc3519c5f660e9defb2de7c7115c
awelvor <philippe.regent@gmail.com>
2024-08-20 08:30:56 +0200

    Create gif2.js

---
+++ assets/js/gif2.js
@@ -0,0 +1,9 @@
+tab = [
+{url:"assets/images/gg.gif", title:"gg"},
+{url:"assets/images/gg1.gif", title:"gg1"},
+{url:"assets/images/gg2.gif", title:"gg2"},
+{url:"assets/images/gg3.gif", title:"gg3"},
+
+
+
+];
