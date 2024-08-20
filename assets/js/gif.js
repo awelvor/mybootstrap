ddcba0088ee4d89f4040d18c43e5d70a9c681d5e
awelvor <philippe.regent@gmail.com>
2024-08-20 08:41:12 +0200

    Update gif.js

--- assets/js/gif.js
+++ assets/js/gif.js
@@ -1,16 +1,18 @@
-const jo = [
-"assets/images/paris-games-artistic-gymnastics-6753651837110525-law.gif",
-"assets/images/paris-games-artistic-swimming-day-2-6753651837110563-law.gif",
-"assets/images/paris-games-breaking-6753651837110566-law.gif",
-"assets/images/paris-games-climbing-6753651837110564-law.gif",
-"assets/images/paris-games-climbing-day-2-6753651837110565-law.gif",
-"assets/images/paris-games-conclude-6753651837110568-law.gif",
-"assets/images/paris-games-football-6753651837110524-law.gif",
-"assets/images/paris-games-july-most-searched-playground-6753651837110527-law.gif",
-"assets/images/paris-games-rings-6753651837110531-law.gif",
-"assets/images/paris-games-sailing-6753651837110529.4-law.gif",
-"assets/images/paris-games-sailing-day-2-6753651837110530-law.gif",
-"assets/images/paris-games-skateboarding-6753651837110523-law.gif",
-"assets/images/paris-games-surfing-6753651837110528.3-law.gif",
-"assets/images/paris-games.png",
+tab = [
+
+
+{url:"assets/images/paris-games/paris-games-artistic-gymnastics-6753651837110525-law.gif", title:"gymnastics"},
+{url:"assets/images/paris-games/paris-games-artistic-swimming-day-2-6753651837110563-law.gif", title:"swimmng"},
+{url:"assets/images/paris-games/paris-games-breaking-6753651837110566-law.gif", title:"breaking"},
+{url:"assets/images/paris-games/paris-games-climbing-6753651837110564-law.gif", title:"climbing"},
+{url:"assets/images/paris-games/paris-games-climbing-day-2-6753651837110565-law.gif", title:"climbing-day-2"},
+{url:"assets/images/paris-games/paris-games-conclude-6753651837110568-law.gif", title:"conclude"},
+{url:"assets/images/paris-games/paris-games-football-6753651837110524-law.gif", title:"football"},
+{url:"assets/images/paris-games/paris-games-july-most-searched-playground-6753651837110527-law.gif", title:"july"},
+{url:"assets/images/paris-games/paris-games-rings-6753651837110531-law.gif", title:"rings"},
+{url:"assets/images/paris-games/paris-games-sailing-6753651837110529.4-law.gif", title:"sailing"},
+{url:"assets/images/paris-games/paris-games-sailing-day-2-6753651837110530-law.gif", title:"sailing-day-2"},
+{url:"assets/images/paris-games/paris-games-skateboarding-6753651837110523-law.gif", title:"skateboarding"},
+{url:"assets/images/paris-games/paris-games-surfing-6753651837110528.3-law.gif", title:"surfing"},
+{url:"assets/images/paris-games/paris-games.png",title:"success"},
 ];
